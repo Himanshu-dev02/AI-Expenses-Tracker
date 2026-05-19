d1fe10c1bebcf7f1b306584bce7609a56486cb2f
Himanshu Meshram <meshramhimanshu20@gmail.com>
2026-05-20 00:17:05 +0530

    Delete config/db.js

--- config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://meshramhimanshu20_db_user:aV7UlzQ2FN0jrLCk@cluster0.hrkhgoh.mongodb.net/ExpensesTracker")
-    .then(() => 
-        console.log("Connected to MongoDB"));
-}
-    
