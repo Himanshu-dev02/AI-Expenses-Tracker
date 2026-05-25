44003944271d96ebed34ffc47c1490ddaa260f08
Himanshu-dev02 <meshramhimanshu20@gmail.com>
2026-05-26 00:42:46 +0530

    Your commit message

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://meshramhimanshu20_db_user:aV7UlzQ2FN0jrLCk@cluster0.hrkhgoh.mongodb.net/ExpensesTracker")
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(() => 
         console.log("Connected to MongoDB"));
 }
