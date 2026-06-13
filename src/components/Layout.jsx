8d4cfa722d4fc3800c6b3c02102e87b9f60282bf
Himanshu-dev02 <meshramhimanshu20@gmail.com>
2026-06-13 23:46:56 +0530

    created dashboard page and Changes in layout.jsx file

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,36 @@ import Navbar from "./Navbar";
 import Sidebar from "./Siderbar";
 
 
+const API_BASE_URL = "http://localhost:4000/api";
+
 const Layout = ({ onLogout, user }) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   
   return (
     <div className={styles.layout.root}>
-        <Sidebar user={user} isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-        <Navbar user={user} onLogout={onLogout}/>
+       <Navbar user={user} onLogout={onLogout}/>
+        <Sidebar user={user} isCollapsed={sidebarCollapsed} setIsCollapsed={setSidebarCollapsed} />
+       
+
+        <div className={styles.layout.mainContainer(sidebarCollapsed)}>
+          <div className={styles.header.Container}>
+          <div>
+            <h1 className={styles.header.title}>Dashboard</h1>
+            <p className={styles.header.subtitle}>Welcome Back </p>
+          </div>
+          </div>
+          <div className={styles.statCards.grid}>
+            <div className={styles.statCards.card}>
+              <div className={styles.statCards.cardHeader}>
+                <div>
+                  <p className={styles.statCards.cardTitle}>Total Balance</p>
+                  <p className={styles.statCards.cardValue}>$ {stats}</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
         
     </div>
   );
