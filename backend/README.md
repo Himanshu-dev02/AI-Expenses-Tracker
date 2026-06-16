ed7bb54db828bc1b36892de8e3aef305c3adc72a
Himanshu Meshram <meshramhimanshu20@gmail.com>
2026-06-16 23:16:11 +0530

    Delete backend directory

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-# AI-Expenses-Tracker
--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authMiddleware from "../middleware/auth.js";
-import {  getDashboardOverview } from "../controllers/dashboardController.js";
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get("/", authMiddleware, getDashboardOverview);
-
-export default dashboardRouter;
