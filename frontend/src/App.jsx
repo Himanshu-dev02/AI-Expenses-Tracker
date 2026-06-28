3c76b6159fe89a38bf07a810f7f6fb0fdc424410
Himanshu-dev02 <meshramhimanshu20@gmail.com>
2026-06-28 22:07:08 +0530

    complete profile page code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@
 import axios from "axios";
 import Income from "./pages/Income";
 import Expense from "./pages/Expense";
+import Profile from "./pages/Profile";
  
 
  const API_URL = "http://localhost:4000";
@@ -243,7 +244,16 @@ import Expense from "./pages/Expense";
            refreshTransactions={refreshTransactions}  />
          } 
          />
+
+         <Route path="/profile" element={
+          <Profile 
+         user={user}
+         onUpdateProfile = {updateUserData}
+          onLogout={handleLogout}/>}/>
         </Route>
+        <Route 
+        path="*"
+        element={<Navigate to={user ? "/" : "/login"} replace/>}/>
       </Routes>
       </>
    )
